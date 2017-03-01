e616d3795d1d7ec6b616b907ee5b7b31e6c848eb
Wesley09 <wesleyoliveira22264@gmail.com>
2017-03-01 16:17:12 -0300

    S7273T: Rename Device S7272 To S7273T

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := GT-S7272
+PRODUCT_RELEASE_NAME := GT-S7273T
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
